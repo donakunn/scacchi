62c7ac30a2e37f158177e6ab25d2d9ba0c9d9638
Shaleed <filippo.iacobellis99@gmail.com>
2020-04-12 09:08:42 +0200

    realizzata versione base di AppMain.java

--- src/main/java/it/uniba/main/AppMain.java
+++ src/main/java/it/uniba/main/AppMain.java
@@ -4,8 +4,12 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.GeneralSecurityException;
+import java.util.Scanner;
+import Partita.Cella;
 
 import it.uniba.sotorrent.GoogleDocsUtils;
+
+enum Colonna {a,b,c,d,e,f,g,h};
 
 /**
  * The main class for the project. It must be customized to meet the project
@@ -28,7 +32,7 @@ public final class AppMain {
 	 * @param args The command-line arguments.
 	 */
 	public static void main(final String[] args) {
-
+		Cella[][] scacchiera= new Cella[8][8];
 		System.out.println("Current working dir: " + System.getProperty("user.dir"));
 
 		if (args.length > 0) {
@@ -52,5 +56,61 @@ public final class AppMain {
 		}
 
 	}
+	
+	void nuovaPartita() {
+		System.out.println("Creazione partita in corso...");
+		for(int j=0;j<8;j++) {
+			//inizializzazione pedoni a2-h2 (lato bianco)
+			//scacchiera[1][j]= new Cella(new Pedone());
+			
+			//inizializzazione pedoni a7-h7 (lato nero)
+			//scacchiera[6][j]= new Cella(new Pedone());
+		};
+		//inizializzazione case a1-h1 (lato bianco)
+		/* scacchiera[0][0]= new Cella(new Torre()); scacchiera[0][1]= new Cella(new
+		 * Cavallo()); scacchiera[0][2]= new Cella(new Alfiere()); scacchiera[0][3]= new
+		 * Cella(new Donna()); scacchiera[0][4]= new Cella(new Re()); scacchiera[0][5]=
+		 * new Cella(new Alfiere()); scacchiera[0][6]= new Cella(new Cavallo());
+		 * scacchiera[0][7]= new Cella(new Torre());
+		 */
+		
+		//inizializzazione case a8-h8 (lato nero)
+		/*
+		 * scacchiera[7][0]= new Cella(new Torre()); scacchiera[7][1]= new Cella(new
+		 * Cavallo()); scacchiera[7][2]= new Cella(new Alfiere()); scacchiera[7][3]= new
+		 * Cella(new Donna()); scacchiera[7][4]= new Cella(new Re()); scacchiera[7][5]=
+		 * new Cella(new Alfiere()); scacchiera[7][6]= new Cella(new Cavallo());
+		 * scacchiera[7][7]= new Cella(new Torre());
+		 */	
+	};
+	
+	void riceviMossa() {
+		Scanner in = new Scanner(System.in);
+		String mossa= in.nextLine();
+		if (mossa.length()==2){
+			//muovi pedone
+		}else {
+			char pezzoScelto=mossa.charAt(0);
+			switch(pezzoScelto) {
+			case 'T':
+				//muovi Torre
+			case 'C':
+				//muovi Cavallo
+			case 'A':
+				//muovi Alfiere
+			case 'D':
+				//muovi Donna
+			case 'R':
+				//muovi Re
+			}
+			mossa.substring(1);
+		}
+		
+		//parsing della colonna in cui muovere
+		Colonna colonnaMossa= Colonna.valueOf(mossa.substring(0,1));
+		//accesso con scacchiera[mossa.charAt(1)-1][colonnaMossa.ordinal()]
+		
+		in.close();
+	};
 
 }
