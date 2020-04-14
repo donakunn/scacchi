b1aecf86d2e99a0d3b6d76f025458ed284c13802
DonatoLucente <d.lucente1393@gmail.com>
2020-04-14 17:14:21 +0200

    Aggiunta implementazione parziale pezzi mancanti

---
+++ src/main/java/Pezzi/Cavallo.java
@@ -0,0 +1,34 @@
+package Pezzi;
+
+import java.util.ArrayList;
+
+import Partita.Cella;
+
+public class Cavallo extends Pezzo {
+
+	
+	public Cavallo(int col, int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.colore = col;
+		if (col == 0) {
+			this.tipoPezzo = "U"+"2658"; //Cavallo bianco
+			
+		}
+		else if (col == 1) {
+			this.tipoPezzo = "U"+"265E"; //Cavallo nero
+			
+			
+		}
+		else throw new IllegalArgumentException("Valore non valido, valori accettati: 0,1");
+
+		
+	}
+
+	@Override
+	public ArrayList<Cella> MosseDisponibili(Cella[][] statoGioco) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/Pezzi/Re.java
@@ -0,0 +1,34 @@
+package Pezzi;
+
+import java.util.ArrayList;
+
+import Partita.Cella;
+
+public class Re extends Pezzo {
+
+	
+	public Re(int col, int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.colore = col;
+		if (col == 0) {
+			this.tipoPezzo = "U"+"2654"; //Re bianco
+			
+		}
+		else if (col == 1) {
+			this.tipoPezzo = "U"+"265A"; //pedone nero
+			
+			
+		}
+		else throw new IllegalArgumentException("Valore non valido, valori accettati: 0,1");
+
+		
+	}
+
+	@Override
+	public ArrayList<Cella> MosseDisponibili(Cella[][] statoGioco) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
