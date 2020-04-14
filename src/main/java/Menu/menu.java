244f6b81171ce74400c9d15dcaeb1047c244d171
Megi Gjata <gjatamegi10@gmail.com>
2020-04-14 12:06:06 +0200

    Ho aggiunto il metodo mostrare elenco comandi

--- src/main/java/Menu/menu.java
+++ src/main/java/Menu/menu.java
@@ -1,5 +1,9 @@
 package Menu;
 
 public class menu {
+	public void mostrareelencocomandi() {
+		System.out.print("Se la partita non è in corso compariranno questi comandi:\n help \n play \n quit\n");
+        System.out.print("Se la partita è in corso compariranno questi comandi:\n help \n quit \n board \n moves \n captures");
+	}
 
 }
