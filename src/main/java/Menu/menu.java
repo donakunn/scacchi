b86f37d86b2f8ca91fc1bed00fa3d6b4cc035f4c
PatrickClark <patrickclark@outlook.it>
2020-04-14 19:05:52 +0200

    Implementato il comando show

--- src/main/java/Menu/menu.java
+++ src/main/java/Menu/menu.java
@@ -2,8 +2,18 @@ package Menu;
 
 public class menu {
 	public void mostrareelencocomandi() {
-		System.out.print("Se la partita non è in corso compariranno questi comandi:\n help \n play \n quit\n");
-        System.out.print("Se la partita è in corso compariranno questi comandi:\n help \n quit \n board \n moves \n captures");
+		System.out.print("Se la partita non ï¿½ in corso compariranno questi comandi:\n help \n play \n quit\n");
+        System.out.print("Se la partita ï¿½ in corso compariranno questi comandi:\n help \n quit \n board \n moves \n captures");
 	}
-
+   
+	public void mostraScacchiera() {
+		Scanner scanner = new Scanner(System.in); // prende il comando scritto in input
+        String comando = scanner.next();
+        int i;
+        int j;
+        if(comando.equals("show")) {// verifica il comando scritto in input e lo confronta con il comando 'show'
+        
+        }
+        }
+        
 }
