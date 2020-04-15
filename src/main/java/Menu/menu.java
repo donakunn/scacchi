3801f7b498305551e5afaaf6d047a1750e58c8cc
patrickclark9 <61475687+patrickclark9@users.noreply.github.com>
2020-04-15 11:07:47 +0200

    Update menu.java
    modificato il comando da 'show' a 'board'

--- src/main/java/Menu/menu.java
+++ src/main/java/Menu/menu.java
@@ -6,10 +6,10 @@ public class menu {
         System.out.print("Se la partita � in corso compariranno questi comandi:\n help \n quit \n board \n moves \n captures");
 	}
  
-	public void comandoShow() {
+	public void comandoBoard() {
 		Scanner scanner = new Scanner(System.in); // prende il comando scritto in input
         String comando = scanner.next();
-        if(comando.equals("show")) // verifica il comando scritto in input e lo confronta con il comando 'show'
+        if(comando.equals("board")) // verifica il comando scritto in input e lo confronta con il comando 'board'
         {
         	//da inserire la funzione di stampa della matrice
         }
