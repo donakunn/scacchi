5a324a74eb1f69365a36b2ec197b2ae67346721f
PatrickClark <patrickclark@outlook.it>
2020-04-29 16:31:50 +0200

    terminata l'implementazione del movimento dell'alfiere

--- src/main/java/it/uniba/main/Menu.java
+++ src/main/java/it/uniba/main/Menu.java
@@ -71,8 +71,18 @@ class Menu {
 				System.err.println("Pezzo non ancora implementato");// muovi Cavallo
 				break;
 			case 'A':
-				System.err.println("Pezzo non ancora implementato");// muovi Alfiere
-				break;
+				if (input.length() == 3) {
+					try {
+						game.moveBishop(input);
+					} catch (IllegalArgumentException e) {
+						System.err.println("Illegal move; Please try again");
+					} catch (IndexOutOfBoundsException e) {
+						System.err.println("Illegal move; Please try again");
+					} catch (IllegalMoveException e) {
+						System.err.println(e.getMessage());
+					}
+					break;
+				}
 			case 'D':
 				if (input.length() == 3) {
 					try {
