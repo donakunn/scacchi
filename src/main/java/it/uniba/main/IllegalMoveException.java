c6ec9c18c9e2d5462600b86e25ce1907a11210fc
mariogiordano1 <61432762+mariogiordano1@users.noreply.github.com>
2020-05-24 14:10:31 +0200

    Creata documentazione javadoc per la classe IllegalMoveException

--- src/main/java/it/uniba/main/IllegalMoveException.java
+++ src/main/java/it/uniba/main/IllegalMoveException.java
@@ -2,17 +2,20 @@ package it.uniba.main;
 
 /**
  * <<no-ECB>><br>
- * Exception class for illegal move cases.
+ * <p>Titolo: IllegalMoveException</p>
+ * <p>Descrizione: La classe IllegalMoveException e' utilizzata per sollevare eccezioni nel caso in cui la mossa specificata 
+ * dall'utente sia una mossa illegale.</p>
  *
  * @author Donato Lucente
  */
 public class IllegalMoveException extends Exception {
 
-    /**
-     *
-     */
     private static final long serialVersionUID = 1L;
 
+    /**
+     * E' il costruttore della classe.
+     * @param s stringa corrispondente all'eccezione.
+     */
     public IllegalMoveException(final String s) {
         super(s);
     }
