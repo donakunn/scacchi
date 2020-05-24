a90136e97ca26e4709fcb7cb9e513629dc645f02
mariogiordano1 <61432762+mariogiordano1@users.noreply.github.com>
2020-05-24 12:16:07 +0200

    Creata documentazione javadoc per la classe Cell

--- src/main/java/it/uniba/main/Cell.java
+++ src/main/java/it/uniba/main/Cell.java
@@ -2,9 +2,10 @@ package it.uniba.main;
 
 /**
  * <<entity>><br>
- * Cell represent a cell in the board attribute of {@link Game}. <br>
- * Each cell contains the piece (if any) and getters and setters for that piece along a toString
- * method for printing the cell.
+ * <p>Titolo: Cell</p>
+ * <p>Descrizione: La classe Cell viene utilizzata per gestire le celle della scacchiera all'interno del gioco. Ogni cella contiene un 
+ * pezzo e puo' essere svuotata o riempita a seconda delle necessita'. Contiene inoltre un metodo che permette di convertire
+ * il pezzo in stringa per poterlo stampare.
  *
  * @author Filippo Iacobellis
  */
@@ -12,22 +13,42 @@ class Cell {
 
     private Piece piece;
 
+    /**
+     * E' il costruttore della classe.
+     * 
+     * @param piece pezzo contenuto nella cella.
+     */
     Cell(final Piece piece) {
         this.piece = piece;
     }
 
+    /**
+     * 
+     * @return pezzo contenuto nella cella.
+     */
     Piece getPiece() {
         return this.piece;
     }
 
+    /**
+     * Svuota la cella assegnando al pezzo il valore null.
+     */
     void setEmpty() {
         this.piece = null;
     }
 
+    /**
+     * Crea il pezzo all'interno della cella.
+     * 
+     * @param piece pezzo contenuto nella cella.
+     */
     void setPiece(final Piece piece) {
         this.piece = piece;
     }
 
+    /**
+     * Converte il pezzo a stringa.
+     */
     public String toString() {
         if (piece == null) {
             return "[ ]";
