aae31c449106f4e73ccb5161ae72c4c5f0b2ac8a
PatrickClark <patrickclark@outlook.it>
2020-05-25 15:44:43 +0200

    corretto errore di checkstyle e formattazione di Cell

--- src/main/java/it/uniba/main/Cell.java
+++ src/main/java/it/uniba/main/Cell.java
@@ -2,61 +2,55 @@ package it.uniba.main;
 
 /**
  * <<entity>><br>
- * <p>Titolo: Cell</p>
- * <p>Descrizione: La classe Cell viene utilizzata per gestire le celle della scacchiera all'interno del gioco. Ogni cella contiene un 
- * pezzo e puo' essere svuotata o riempita a seconda delle necessita'. Contiene inoltre un metodo che permette di convertire
- * il pezzo in stringa per poterlo stampare.
+ *
+ * <p>Titolo: Cell
+ *
+ * <p>Descrizione: La classe Cell viene utilizzata per gestire le celle della scacchiera all'interno
+ * del gioco. Ogni cella contiene un pezzo e puo' essere svuotata o riempita a seconda delle
+ * necessita'. Contiene inoltre un metodo che permette di convertire il pezzo in stringa per poterlo
+ * stampare.
  *
  * @author Filippo Iacobellis
  */
 class Cell {
 
-	/**
-	 * Si riferisce al pezzo da inserire o eliminare nella cella.
-	 */
-    private Piece piece;
+  /** Si riferisce al pezzo da inserire o eliminare nella cella. */
+  private Piece piece;
 
-    /**
-     * E' il costruttore della classe.
-     * 
-     * @param piece pezzo contenuto nella cella.
-     */
-    Cell(final Piece piece) {
-        this.piece = piece;
-    }
+  /**
+   * E' il costruttore della classe.
+   *
+   * @param p pezzo contenuto nella cella.
+   */
+  Cell(final Piece p) {
+    this.piece = p;
+  }
 
-    /**
-     * 
-     * @return pezzo contenuto nella cella.
-     */
-    Piece getPiece() {
-        return this.piece;
-    }
+  /** @return pezzo contenuto nella cella. */
+  Piece getPiece() {
+    return this.piece;
+  }
 
-    /**
-     * Svuota la cella assegnando al pezzo il valore null.
-     */
-    void setEmpty() {
-        this.piece = null;
-    }
+  /** Svuota la cella assegnando al pezzo il valore null. */
+  void setEmpty() {
+    this.piece = null;
+  }
 
-    /**
-     * Crea il pezzo all'interno della cella.
-     * 
-     * @param piece pezzo contenuto nella cella.
-     */
-    void setPiece(final Piece piece) {
-        this.piece = piece;
-    }
+  /**
+   * Crea il pezzo all'interno della cella.
+   *
+   * @param p pezzo contenuto nella cella.
+   */
+  void setPiece(final Piece p) {
+    this.piece = p;
+  }
 
-    /**
-     * Converte il pezzo a stringa.
-     */
-    public String toString() {
-        if (piece == null) {
-            return "[ ]";
-        } else {
-            return "[" + piece.toString() + "]";
-        }
+  /** Converte il pezzo a stringa. */
+  public String toString() {
+    if (piece == null) {
+      return "[ ]";
+    } else {
+      return "[" + piece.toString() + "]";
     }
+  }
 }
