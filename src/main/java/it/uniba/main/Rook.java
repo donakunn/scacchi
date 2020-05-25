6e994e929404d503b3c3798e3178da01ad01d91a
DonatoLucente <d.lucente1393@gmail.com>
2020-05-25 17:06:29 +0200

    fix dead code

--- src/main/java/it/uniba/main/Rook.java
+++ src/main/java/it/uniba/main/Rook.java
@@ -93,14 +93,14 @@ class Rook extends Piece {
         } else { // Non valido
             return false;
         }
-        if(i!=a||j!=b) {
-        	return false;
-        }
-        if (Game.getCell(i, j).getPiece() == null
-                || Game.getCell(i, j).getPiece().getColor() != blackTurnColor) {
-            return true;
+        if((i==a)&&(j==b)) {
+        	if (Game.getCell(i, j).getPiece() == null
+                    || Game.getCell(i, j).getPiece().getColor() != blackTurnColor) {
+                return true;
+            }
         }
         return false;
+        
     }
 
 
